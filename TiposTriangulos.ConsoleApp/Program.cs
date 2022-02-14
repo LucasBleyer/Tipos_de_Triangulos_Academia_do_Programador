2f3fee2975d339da794056d3c07e5eb86dedfed9
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-14 15:02:46 -0300

    triangulo invalido

--- TiposTriangulos.ConsoleApp/Program.cs
+++ TiposTriangulos.ConsoleApp/Program.cs
@@ -15,22 +15,25 @@ namespace TiposTriangulos.ConsoleApp
                     Console.Write(i + "º lado: ");
                     lados[i] = Convert.ToInt32(Console.ReadLine());
                 } while (lados[i] < 0);
-
-                if ((lados[1] < lados[2] + lados[3]) && (lados[2] < lados[1] + lados[3]) && (lados[3] < lados[1] + lados[2]))
+            }
+            if ((lados[1] < lados[2] + lados[3]) || (lados[2] < lados[1] + lados[3]) || (lados[3] < lados[1] + lados[2]))
+            {
+                if ((lados[1] == lados[2]) && (lados[1] == lados[3]) && (lados[2] == lados[3]))
                 {
-                    if ((lados[1] == lados[2]) && (lados[1] == lados[3]) && (lados[2] == lados[3]))
-                    {
-                        Console.WriteLine("Triângulo Equilátero!");
-                    }
-                    else if ((lados[1] != lados[2]) && (lados[2] != lados[3]) && (lados[1] != lados[3]))
-                    {
-                        Console.WriteLine("Triângulo Escaleno!");
-                    }
-                    else if ((lados[1] == lados[2]) || (lados[2] == lados[3]) || (lados[1] == lados[3]))
-                    {
-                        Console.WriteLine("Triangulo Isósceles!");
-                    }
+                    Console.WriteLine("Triângulo Equilátero!");
                 }
+                else if ((lados[1] != lados[2]) && (lados[2] != lados[3]) && (lados[1] != lados[3]))
+                {
+                    Console.WriteLine("Triângulo Escaleno!");
+                }
+                else if ((lados[1] == lados[2]) || (lados[2] == lados[3]) || (lados[1] == lados[3]))
+                {
+                    Console.WriteLine("Triangulo Isósceles!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Triângulo Inválido");
             }
         }
     }
