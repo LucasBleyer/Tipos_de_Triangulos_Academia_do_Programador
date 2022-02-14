f902356ed97860ffeae0815c430029484436319c
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-14 15:22:05 -0300

    finalizado

--- TiposTriangulos.ConsoleApp/Program.cs
+++ TiposTriangulos.ConsoleApp/Program.cs
@@ -6,35 +6,55 @@ namespace TiposTriangulos.ConsoleApp
     {
         static void Main(string[] args)
         {
-            int[] lados = new int[4];
+            char opcao;
+            do
+            {
+                Console.WriteLine("\r\n---------------------------------------------------");
+                Console.WriteLine("Verificar Triângulo");
+                int[] lados = new int[4];
+                for (int i = 1; i < lados.Length; i++)
+                {
+                    do
+                    {
+                        Console.Write(i + "º lado: ");
+                        lados[i] = Convert.ToInt32(Console.ReadLine());
+                    } while (lados[i] < 0);
+                }
 
-            for (int i = 1; i < lados.Length; i++)
-            {
-                do
+                if ((lados[1] < lados[2] + lados[3]) || (lados[2] < lados[1] + lados[3]) || (lados[3] < lados[1] + lados[2]))
                 {
-                    Console.Write(i + "º lado: ");
-                    lados[i] = Convert.ToInt32(Console.ReadLine());
-                } while (lados[i] < 0);
-            }
-            if ((lados[1] < lados[2] + lados[3]) || (lados[2] < lados[1] + lados[3]) || (lados[3] < lados[1] + lados[2]))
-            {
-                if ((lados[1] == lados[2]) && (lados[1] == lados[3]) && (lados[2] == lados[3]))
-                {
-                    Console.WriteLine("Triângulo Equilátero!");
+                    if ((lados[1] == lados[2]) && (lados[1] == lados[3]) && (lados[2] == lados[3]))
+                    {
+                        Console.WriteLine("\r\nTriângulo Equilátero!");
+                    }
+                    else if ((lados[1] != lados[2]) && (lados[2] != lados[3]) && (lados[1] != lados[3]))
+                    {
+                        Console.WriteLine("\r\nTriângulo Escaleno!");
+                    }
+                    else if ((lados[1] == lados[2]) || (lados[2] == lados[3]) || (lados[1] == lados[3]))
+                    {
+                        Console.WriteLine("\r\nTriangulo Isósceles!");
+                    }
                 }
-                else if ((lados[1] != lados[2]) && (lados[2] != lados[3]) && (lados[1] != lados[3]))
+                else
                 {
-                    Console.WriteLine("Triângulo Escaleno!");
+                    Console.WriteLine("Triângulo Inválido");
                 }
-                else if ((lados[1] == lados[2]) || (lados[2] == lados[3]) || (lados[1] == lados[3]))
+
+                Console.WriteLine("Deseja para Verificar mais um Tipo de Triângulo?");
+                Console.WriteLine("[S]Sim  [N]Não");
+                opcao = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (opcao == 'n' || opcao == 'N')
                 {
-                    Console.WriteLine("Triangulo Isósceles!");
+                    break;
                 }
-            }
-            else
-            {
-                Console.WriteLine("Triângulo Inválido");
-            }
+                else
+                {
+                    continue;
+                }
+
+            } while (opcao != 1);
         }
     }
 }
